3db4563178c94ed2fa052aba40bab057790c2d58
Robert Hecker <roberthecker@gmx.net>
2025-12-28 01:16:29 +0100

    Run tests for comparing Rust and Python inference results on all files inside test data directory

--- src/crepe.rs
+++ src/crepe.rs
@@ -116,9 +116,9 @@ impl CrepeModel {
 
 #[cfg(test)]
 mod tests {
-    use float_cmp::assert_approx_eq;
     use crate::crepe::*;
     use crate::ONNX_MODEL_PATH;
+    use float_cmp::assert_approx_eq;
 
     #[test]
     fn test_cents_mapping() {
@@ -135,9 +135,28 @@ mod tests {
         let session = Session::builder()?.commit_from_file(ONNX_MODEL_PATH)?;
         let crepe_model = CrepeModel::new(session);
 
-        let sample_bytes = std::fs::read("test-data/sweep_samples.npy")?;
-        let frequency_bytes = std::fs::read("test-data/sweep_frequencies.npy")?;
-        let confidence_bytes = std::fs::read("test-data/sweep_confidences.npy")?;
+        let wav_files: Vec<std::path::PathBuf> = std::fs::read_dir("test-data")?
+            .into_iter()
+            .filter(|r| r.is_ok())
+            .map(|r| r.unwrap().path())
+            .filter(|r| r.is_file() && r.file_name().unwrap().to_string_lossy().ends_with(".wav"))
+            .collect();
+        for file in wav_files {
+            do_test_predict_single(&crepe_model, &file)?;
+        }
+
+        Ok(())
+    }
+
+    fn do_test_predict_single(
+        crepe_model: &CrepeModel,
+        wav_file: &std::path::PathBuf,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let parent_dir = wav_file.parent().unwrap().to_string_lossy();
+        let stem = wav_file.file_stem().unwrap().to_string_lossy();
+        let sample_bytes = std::fs::read(format!("{}/{}_samples.npy", parent_dir, stem))?;
+        let frequency_bytes = std::fs::read(format!("{}/{}_frequencies.npy", parent_dir, stem))?;
+        let confidence_bytes = std::fs::read(format!("{}/{}_confidences.npy", parent_dir, stem))?;
 
         let sample_npy = npyz::NpyFile::new(&sample_bytes[..])?;
         let frequency_npy = npyz::NpyFile::new(&frequency_bytes[..])?;
@@ -148,21 +167,41 @@ mod tests {
         let frequency_data = frequency_npy.into_vec::<f64>()?;
         let confidence_data = confidence_npy.into_vec::<f32>()?;
 
-        const U16_MAX: f32 = u16::MAX as f32;
-        // TODO: build chunks of 1024 samples instead of only a single chunk.
-        let i16_samples: [i16; SAMPLES_PER_STEP] = sample_data
-            .iter()
-            .map(|sample| ((U16_MAX / 2.0) * sample) as i16)
-            .take(SAMPLES_PER_STEP)
-            .collect::<Vec<i16>>()
-            .try_into()
-            .expect("There should be 1024 samples.");
-        let prediction = crepe_model.predict_single(i16_samples);
+        // We only take full 1024-sample frames, so we discard the fractional part.
+        let frame_count = (sample_data.len() as f32 / SAMPLES_PER_STEP as f32).floor() as usize;
+        assert_eq!(frame_count, frequency_data.len());
+        assert_eq!(frame_count, confidence_data.len());
 
-        // A rather large epsilon, but fine for our use case.
-        const EPSILON: f32 = 0.001;
-        assert_approx_eq!(f32, prediction.frequency, frequency_data[0] as f32, epsilon = EPSILON);
-        assert_approx_eq!(f32, prediction.confidence, confidence_data[0], epsilon = EPSILON);
+        const U16_MAX: f32 = u16::MAX as f32;
+        let i16_sample_chunks: Vec<[i16; SAMPLES_PER_STEP]> = sample_data
+            .chunks_exact(SAMPLES_PER_STEP)
+            .map(|chunk| {
+                chunk
+                    .iter()
+                    .map(|sample| ((U16_MAX / 2.0) * sample) as i16)
+                    .collect::<Vec<i16>>()
+                    .try_into()
+                    .unwrap()
+            })
+            .collect::<Vec<[i16; SAMPLES_PER_STEP]>>();
+        for (i, sample_chunk) in i16_sample_chunks.iter().enumerate() {
+            let prediction = crepe_model.predict_single(*sample_chunk);
+
+            // A rather large epsilon, but fine for our use case.
+            const EPSILON: f32 = 0.01;
+            assert_approx_eq!(
+                f32,
+                prediction.frequency,
+                frequency_data[i] as f32,
+                epsilon = EPSILON
+            );
+            assert_approx_eq!(
+                f32,
+                prediction.confidence,
+                confidence_data[i],
+                epsilon = EPSILON
+            );
+        }
 
         Ok(())
     }
